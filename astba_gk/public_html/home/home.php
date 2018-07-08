121f90e32c8592d874f234c6864aeaa22094a2eb
kz12oleg12 <kz12oleg12@gmail.com>
2018-07-08 13:06:45 +0600

    123

---
+++ astba_gk/public_html/home/home.php
@@ -0,0 +1,107 @@
+<link rel="stylesheet" href="/css/style/home.css">
+<section id="home">
+    <div class="home_ld">
+        <div class="hm_content container">
+            <div>
+                <div class="hm_cn">
+                    <div class="name">
+                        <h2>Vip чарет</h2>
+                    </div>
+                    <div class="hm_line"></div>
+                    <div class="text">
+                        <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной
+                            "рыбой" для текстов на латинице с начала XVI века. Lorem Ipsum - это текст-"рыба", часто используемый
+                            в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала
+                            XVI века. </p>
+                    </div>
+                </div>
+                <div class="owl-carousel home_sl owl-theme">
+                    <div class="item">
+                        <span>
+                            <img src="/img/icon/1.png" alt="">
+                            <p>
+                                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            </p>
+                        </span>
+                    </div>
+                    <div class="item">
+                        <span>
+                            <img src="/img/icon/2.png" alt="">
+                            <p>
+                                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            </p>
+                        </span>
+                    </div>
+                    <div class="item">
+                        <span>
+                            <img src="/img/icon/3.png" alt="">
+                            <p>
+                                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            </p>
+                        </span>
+                    </div>
+                    <div class="item">
+                        <span>
+                            <img src="/img/icon/4.png" alt="">
+                            <p>
+                                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            </p>
+                        </span>
+                    </div>
+                    <div class="item">
+                        <span>
+                            <img src="/img/icon/5.png" alt="">
+                            <p>
+                                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            </p>
+                        </span>
+                    </div>
+                    <div class="item">
+                        <span>
+                            <img src="/img/icon/6.png" alt="">
+                            <p>
+                                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            </p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="hm-feed">
+        <div class="name">
+            <h2>Запростить</h2>
+            <h2> стоимость VIP полета </h2>
+        </div>
+        <form action="">
+            <div class="row no-gutters">
+                <div class="col-12 col-md col-lg">
+                    <div class="inp">
+                        <div class="inp-icon">
+                            <!-- <i class="icon ion-md-person fa-lg"></i> -->
+                            <i class="pe-7s-user fa-lg"></i>
+                        </div>
+                        <input type="text" placeholder="Ваше имя">
+                    </div>
+                </div>
+                <div class="col-12 col-md col-lg">
+                    <div class="inp">
+                        <div class="inp-icon">
+                            <!-- <i class="icon ion-md-call fa-lg"></i> -->
+                            <i class="pe-7s-call fa-lg"></i>
+                        </div>
+                        <input type="text" placeholder="Телефон">
+                    </div>
+                </div>
+                <div class="col-12 col-md col-lg">
+                    <div class="inp">
+                        <input type="button" class="bl_1_btnn" value="БЕСПЛАТНЫЙ ПОДБОР РЕЙСА">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+
+<script src="/js/resize.js"></script>
---
+++ astba_gk/public_html/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Hello Default">
+	<meta name="keywords" content="hello, default">
+	<link rel="shortcut icon" href="/default/logo.ico">
+	<title>Hello Default</title>
+
+	<!-- STYLE -->
+	<link href="/css/asti.css" rel="stylesheet">
+	<link href="/css/dropdown.css" rel="stylesheet">
+	<link href="/css/mobile.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/modules/owlcarousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="/modules/owlcarousel/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="/css/animate.css">
+	<link rel="stylesheet" href="/modules/custom-scrollbar/jquery.mCustomScrollbar.css" />
+
+	<!-- ICON FONT -->
+	<link href="/modules/font-awesome/css/font-awesome.css" rel="stylesheet">
+	<link href="https://unpkg.com/ionicons@4.1.1/dist/css/ionicons.min.css" rel="stylesheet">
+	<link href="/modules/pe-icon-7-stroke/css/helper.css" rel="stylesheet">
+	<link href="/modules/pe-icon-7-stroke/css/pe-icon-7-stroke.css" rel="stylesheet">
+	<link rel="stylesheet" href="/modules/flag-icon-css-master/css/flag-icon.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<!-- FONT -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,600,700,900" rel="stylesheet">
+
+
+	<!-- JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+	
+	<script src="/js/jquery.waypoints.min.js"></script>
+	<script src="/modules/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+	<script src="/modules/owlcarousel/owl.carousel.min.js"></script>
+	<script src="/js/index.js"></script>
+
+	<style>
+		body {
+			font-family: 'Open Sans', sans-serif;
+			/* overflow: hidden; */
+			background: #f3f3f3;
+		}
+
+		body.stop {
+			overflow: hidden;
+		}
+
+		@media (max-width:992px) {
+			body {
+				overflow: inherit;
+			}
+		}
+	</style>
+</head>
+
+<body>
+	<?php // in('ta'); ?>
+	<!-- TOP -->
+	<?php require_once("top/header.php"); ?>
+	<?php require_once("top/nav.php"); ?>
+
+	<!-- HOME -->
+	<?php require_once("home/home.php"); ?>
+
+	<!-- BLOCKS -->
+	<?php require_once("blocks/about.php"); ?>
+
+
+	
+
+	<script>
+		
+	</script>
+</body>
+
+</html>
+<?php
+function in($a)
+	{
+	require_once('php/'.$a.'.php');
+	}
+?>
