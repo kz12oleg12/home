9d08a861312fc933160385e299aee66a4a7c7d58
kz12oleg12 <39411312+kz12oleg12@users.noreply.github.com>
2018-07-09 17:12:08 +0600

    123

--- astba_d/public_html/top/nav.php
+++ astba_d/public_html/top/nav.php
@@ -4,50 +4,50 @@
     <div class="container">
         <div class="navbar">
             <ul class="navbar-list md">
-                <a href="#bl_1" class="d-none d-lg-block">
+                <a href="#bl_1" class="d-none">
                     <li class="nav-btn next-sl">
                         <i class="icon ion-md-arrow-down"></i>
                     </li>
                 </a>
-                <li class="nv-cl d-lg-none">
+                <li class="nv-cl">
                     <div class="nav-close">
-                        <i class="icon ion-ios-close fa-2x d-lg-none"></i>
+                        <i class="icon ion-ios-close fa-2x "></i>
                         Menu
                     </div>
                 </li>
                 <li>
                     <a href="#bl_2">
-                        <i class="icon ion-ios-remove fa-lg d-lg-none"></i>
+                        <i class="icon ion-ios-remove fa-lg "></i>
                         Vip Чартер</a>
                 </li>
                 <li>
                     <a href="#bl_3">
-                        <i class="icon ion-ios-remove fa-lg d-lg-none"></i>
+                        <i class="icon ion-ios-remove fa-lg "></i>
                         Групповой Чартер</a>
                 </li>
                 <li>
                     <a href="#bl_4">
-                        <i class="icon ion-ios-remove fa-lg d-lg-none"></i>
+                        <i class="icon ion-ios-remove fa-lg "></i>
                         Медицинский Чартер</a>
                 </li>
                 <li>
                     <a href="#bl_5">
-                        <i class="icon ion-ios-remove fa-lg d-lg-none"></i>
+                        <i class="icon ion-ios-remove fa-lg "></i>
                         Парк самлетов</a>
                 </li>
                 <li>
                     <a href="#bl_6">
-                        <i class="icon ion-ios-remove fa-lg d-lg-none"></i>
+                        <i class="icon ion-ios-remove fa-lg "></i>
                         Парк вертолетов</a>
                 </li>
                 <li>
                     <a href="#bl_7">
-                        <i class="icon ion-ios-remove fa-lg d-lg-none"></i>
+                        <i class="icon ion-ios-remove fa-lg "></i>
                         Менеджмент ВС</a>
                 </li>
                 <li>
                     <a href="#bl_8">
-                        <i class="icon ion-ios-remove fa-lg d-lg-none"></i>
+                        <i class="icon ion-ios-remove fa-lg "></i>
                         Комплексные решения</a>
                 </li>
             </ul>
